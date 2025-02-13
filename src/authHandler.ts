9378c2ddf0a56e85929510ae4d79289d4bd0a5a9
Andy Grüning <andygruening@gmail.com>
2025-02-13 08:51:08 +0100

    added project id to wrangler.toml

--- src/authHandler.ts
+++ src/authHandler.ts
@@ -3,12 +3,13 @@ import {get} from "./httpClient";
 
 class AuthHandlerImpl {
     async verifyRequest(context: any): Promise<string> {
+        const projectId = context.env.project_id;
         const authHeader = context.request.headers.get("Authorization") as string;
         const token = authHeader.split(' ')[1];
-        return await AuthHandler.verifyJwt(token);
+        return await AuthHandler.verifyJwt(token, projectId);
     }
 
-    async verifyJwt(token: string): Promise<string> {
+    async verifyJwt(token: string, projectId: string): Promise<string> {
         const response = await get("https://waas.sequence.app/.well-known/jwks.json", undefined);
         const jwks= response.data as {keys: JWK[]};
 
@@ -19,7 +20,7 @@ class AuthHandlerImpl {
             throw new Error("No signing key found with matching kid");
         }
 
-        const audience = `https://sequence.build/project/${158}`;
+        const audience = `https://sequence.build/project/${projectId}`;
         const verified = await jwtVerify(token, signingKey, { audience });
         return verified.payload.sub;
     }
